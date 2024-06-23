fde4de66ceaa93882622a63cb1fc984d22ad7a34
yqt2000 <brandontsai2000@gmail.com>
2024-06-23 13:09:52 +0800

    Use CMWQ method to modify server

--- http_server.c
+++ http_server.c
@@ -4,10 +4,12 @@
 #include <linux/sched/signal.h>
 #include <linux/tcp.h>
 
+#include <linux/workqueue.h>
 #include "http_parser.h"
 #include "http_server.h"
 
 #define CRLF "\r\n"
+#define KBUILD_MODNAME "khttpd"
 
 #define HTTP_RESPONSE_200_DUMMY                               \
     ""                                                        \
@@ -38,6 +40,9 @@ struct http_request {
     char request_url[128];
     int complete;
 };
+
+struct http_service daemon = {.is_stopped = false};
+struct workqueue_struct *khttp_wq;  // set up workqueue
 
 static int http_server_recv(struct socket *sock, char *buf, size_t size)
 {
@@ -75,7 +80,7 @@ static int http_server_send(struct socket *sock, const char *buf, size_t size)
 
 static int http_server_response(struct http_request *request, int keep_alive)
 {
-    char *response;
+    char const *response;
 
     pr_info("requested_url = %s\n", request->request_url);
     if (request->method != HTTP_GET)
@@ -133,6 +138,7 @@ static int http_parser_callback_body(http_parser *parser,
     return 0;
 }
 
+// 設定 call back function 的部份，主要是用來送出回應 client 的資料
 static int http_parser_callback_message_complete(http_parser *parser)
 {
     struct http_request *request = parser->data;
@@ -145,6 +151,7 @@ static int http_server_worker(void *arg)
 {
     char *buf;
     struct http_parser parser;
+    // 1. 設定 callback function
     struct http_parser_settings setting = {
         .on_message_begin = http_parser_callback_message_begin,
         .on_url = http_parser_callback_request_url,
@@ -168,46 +175,121 @@ static int http_server_worker(void *arg)
     request.socket = socket;
     http_parser_init(&parser, HTTP_REQUEST);
     parser.data = &request;
+
+    // 2. 進到迴圈，使用函式 kthread_should_stop 判斷該執行緒是否該中止
     while (!kthread_should_stop()) {
+        // 3. 接收資料
         int ret = http_server_recv(socket, buf, RECV_BUFFER_SIZE - 1);
         if (ret <= 0) {
             if (ret)
                 pr_err("recv error: %d\n", ret);
             break;
         }
+        // 4. 解析收到的資料
         http_parser_execute(&parser, &setting, buf, ret);
         if (request.complete && !http_should_keep_alive(&parser))
             break;
         memset(buf, 0, RECV_BUFFER_SIZE);
     }
+    // 5. 中斷連線後釋放用到的所有記憶體
     kernel_sock_shutdown(socket, SHUT_RDWR);
     sock_release(socket);
     kfree(buf);
     return 0;
 }
 
+
+static void http_worker(struct work_struct *work)
+{
+    // 透過 container_of 找到結構中的 struct work_struct http_work
+    struct http_server *worker =
+        container_of(work, struct http_server, http_work);
+    // 建立 socket 連線任務與對應 worker 處理
+    http_server_worker(worker->sock);
+}
+
+// ref : kecho create_work
+static struct work_struct *create_work(struct socket *sk)
+{
+    struct http_server *client;
+    // GFP_KERNEL the flag of allocation
+    // https://elixir.bootlin.com/linux/latest/source/include/linux/gfp.h#L341
+    client = kmalloc(sizeof(struct http_server), GFP_KERNEL);
+
+    if (!client)
+        return NULL;
+
+    client->sock = sk;
+
+    INIT_WORK(&client->http_work, http_worker);
+    list_add(&client->list, &daemon.worker);
+    return &client->http_work;
+}
+
+static void free_work(void)
+{
+    struct http_server *tmp, *target;
+    // list : member
+    list_for_each_entry_safe (target, tmp, &daemon.worker, list) {
+        kernel_sock_shutdown(target->sock, SHUT_RDWR);
+        flush_work(&target->http_work);
+        sock_release(target->sock);
+        kfree(target);
+    }
+}
+
 int http_server_daemon(void *arg)
 {
     struct socket *socket;
-    struct task_struct *worker;
+    // struct task_struct *worker;
     struct http_server_param *param = (struct http_server_param *) arg;
 
+    // CMWQ
+    struct work_struct *work;
+    khttp_wq = alloc_workqueue("khttp_wq", WQ_UNBOUND, 0); /* workqueue.h API*/
+    if (!khttp_wq)
+        return -ENOMEM;
+    // initial workqueue head
+    INIT_LIST_HEAD(&daemon.worker);
+
+    // 登記要接收的 signal
     allow_signal(SIGKILL);
     allow_signal(SIGTERM);
 
+    // 判斷執行緒是否該被中止
     while (!kthread_should_stop()) {
-        int err = kernel_accept(param->listen_socket, &socket, 0);
+        int err = kernel_accept(param->listen_socket, &socket,
+                                0);  // 接受 client 連線要求
         if (err < 0) {
+            // 檢查當前執行緒是否有 signal 發生
             if (signal_pending(current))
                 break;
             pr_err("kernel_accept() error: %d\n", err);
             continue;
         }
-        worker = kthread_run(http_server_worker, socket, KBUILD_MODNAME);
-        if (IS_ERR(worker)) {
-            pr_err("can't create more worker process\n");
+        // 建立新的執行緒並且執行函式 http_server_worker
+        // worker = kthread_run(http_server_worker, socket, KBUILD_MODNAME);
+        // if (IS_ERR(worker)) {
+        //     pr_err("can't create more worker process\n");
+        //     continue;
+        // }
+
+        // CMWQ 為每一個連線的請求建立一個 work 進行處理
+        if (unlikely(!(work = create_work(socket)))) {
+            pr_err("can't create work\n");
             continue;
         }
+        // 而建立出來的 work 會由 os 分配 worker 執行，
+        // 配置後由 khttp_wq 將每一個 work 用 list_head 的 linked list
+        // 進行管理， 使用到 queue_work() 將 work 放入 workqueue 中
+        queue_work(khttp_wq, work);
     }
+
+    daemon.is_stopped = true;
+
+    // free work and workqueue
+    free_work();
+    destroy_workqueue(khttp_wq);
+
     return 0;
 }
--- http_server.h
+++ http_server.h
@@ -1,10 +1,28 @@
 #ifndef KHTTPD_HTTP_SERVER_H
 #define KHTTPD_HTTP_SERVER_H
 
+
+#include <linux/module.h>
+#include <linux/workqueue.h>
 #include <net/sock.h>
+
+#define MODULE_NAME "khttpd"
 
 struct http_server_param {
     struct socket *listen_socket;
+};
+
+// manage workqueue
+struct http_service {
+    bool is_stopped;
+    struct list_head worker;
+};
+
+// manage work
+struct http_server {
+    struct socket *sock;
+    struct list_head list;
+    struct work_struct http_work;
 };
 
 extern int http_server_daemon(void *arg);
